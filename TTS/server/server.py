902a5d72f060bbd032cc29fb196b8cd2713a4f8e
eeshaSh <55664746+eeshaSh@users.noreply.github.com>
2026-01-22 12:06:12 -0800

    Nvm, can use :: for all addresses

--- TTS/server/server.py
+++ TTS/server/server.py
@@ -251,7 +251,7 @@ def mary_tts_api_process():
 
 
 def main():
-    app.run(debug=args.debug, host="0.0.0.0", port=args.port)
+    app.run(debug=args.debug, host="::", port=args.port)
 
 
 if __name__ == "__main__":
